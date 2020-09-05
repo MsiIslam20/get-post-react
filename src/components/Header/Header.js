39fddecaab72888bc6d5fb74c2e17297a0fff34a
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-09-05 10:31:49 +0600

    updates

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header fixed-top">
+            <img src={logo} alt=""/>
+            <div className="menu">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Post from '../Post/Post';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
+            <Header />
             <Post />
         </div>
     );

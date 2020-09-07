2d9544a5d9fb56111cb595e2c2f746afe29b5af4
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-09-07 21:39:12 +0600

    updates

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,14 +4,22 @@ import './Comments.css'
 
 const Comments = (props) => {
     
-    const {name, body, email} = props.comments;
+    let {name, body, email, id} = props.comments;
+
+    //generate random number for image
+    if(id > 100){
+
+        const lastDigit = id % 10 ;
+        id = Math.round((id / 10) + lastDigit) ;
+       
+      }
 
     return (
         <Grid container>
             <Box className="comments-inner">
                 <Box justifyContent="flex-start" display="flex">
                     <div>
-                        <p className="img"></p>
+                        <img className="img" src={`https://randomuser.me/api/portraits/men/${id}.jpg`} alt=""/>
                     </div>
                     <div className="title">
                         <h5>{name}</h5>
